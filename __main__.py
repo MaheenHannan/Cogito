46f42e83b596880946443a339e1f8e3e20dabba9
Maheen Hannan <mahin.hannan@outlook.com>
2022-08-10 15:31:20 +0100

    Update

--- __main__.py
+++ __main__.py
@@ -3,10 +3,8 @@ import pulumi
 import pulumi_aws as aws
 import pulumi_docker as docker
 
-# [Placeholder 1: Create a private ECR registry.]
 repo = aws.ecr.Repository('my-repo')
 
-# [Placeholder 2: Get registry info (creds and endpoint).]
 def getRegistryInfo(rid):
     creds = aws.ecr.get_credentials(registry_id=rid)
     decoded = base64.b64decode(creds.authorization_token).decode()
@@ -17,15 +15,12 @@ def getRegistryInfo(rid):
 image_name = repo.repository_url
 registry_info = repo.registry_id.apply(getRegistryInfo)
 
-image_name = repo.repository_url
-registry_info = None # use ECR credentials helper.
-
-# [Placeholder 3: Build and publish the container image.]
 image = docker.Image('my-image',
     build='src/web',
     image_name=image_name,
     registry=registry_info,
 )
 
+# Export the base and specific version image name.
 pulumi.export('baseImageName', image.base_image_name)
 pulumi.export('fullImageName', image.image_name)
